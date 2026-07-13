0bee3fe3e205c47dc1b2c99c6fc7f3ace706981f
EV <bryzgalov.e@yahoo.com>
2026-07-13 19:39:57 -0400

    one more version fix

--- migrations/alembic/versions/0099_add_coupons.py
+++ migrations/alembic/versions/0099_add_coupons.py
@@ -20,7 +20,7 @@ from alembic import op
 
 
 revision: str = '0099'
-down_revision: Union[str, None] = '0094'
+down_revision: Union[str, None] = '0098'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
--- migrations/alembic/versions/0100_add_recurrent_payments.py
+++ migrations/alembic/versions/0100_add_recurrent_payments.py
@@ -24,8 +24,8 @@ import sqlalchemy as sa
 from alembic import op
 
 
-revision: str = '0096'
-down_revision: Union[str, None] = '0100'
+revision: str = '0100'
+down_revision: Union[str, None] = '0099'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
